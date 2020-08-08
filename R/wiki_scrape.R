e486f1af9d9f5f00d8ebd031db06d56d6070c6b3
jamamel <jamamel@gmail.com>
2020-08-08 11:34:07 +0000

    parsing of start dates

--- R/wiki_scrape.R
+++ R/wiki_scrape.R
@@ -3,10 +3,13 @@
 #'
 #' @param url url to wiki page from which to extract cabinet information
 #'
+#' @examples
+#' url <- xml2::read_html('https://es.wikipedia.org/wiki/Anexo:Gabinete_de_Benito_Ju%C3%A1rez')
+#'
 #' @export
 scrape_cabinet <- function(url){
 
-  url %>%
+  x <- url %>%
     rvest::html_node('body') %>%
     rvest::html_nodes('li') %>%
     purrr::keep(~ {.x %>%
@@ -27,12 +30,28 @@ scrape_cabinet <- function(url){
     .[, c('start', 'end') := data.table::tstrsplit(timeframe,
                                                    split = ' (al|y) ')] %>%
     .[, 'end' := end %>% date_trans()] %>%
-    .[, 'start' := start]
+    .[, 'end_year' := lubridate::year(end)] %>%
+    .[, 'end_month' := lubridate::month(end)] %>%
+    .[, 'start' := ifelse(nchar(start) <= 2,
+                          glue::glue('{start} de {dplyr::case_when(
 
+                                                  .$end_month == 1 ~ "enero",
+                                                  .$end_month == 2 ~ "febrero",
+                                                  .$end_month == 3 ~ "marzo",
+                                                  .$end_month == 4 ~ "abril",
+                                                  .$end_month == 5 ~ "mayo",
+                                                  .$end_month == 6 ~ "junio",
+                                                  .$end_month == 7 ~ "julio",
+                                                  .$end_month == 8 ~ "agosto",
+                                                  .$end_month == 9 ~ "septiembre",
+                                                  .$end_month == 10 ~ "octubre",
+                                                  .$end_month == 11 ~ "noviembre",
+                                                  .$end_month == 12 ~ "diciembre",
+                                       )}'),
+                          start)] %>%
+    .[, 'start' := ifelse(stringr::str_detect(start, '\\d{4}'),
+                          start,
+                          glue::glue('{start} de {.$end_year}')) %>%
+        date_trans()]
 
 }
-
-
-# bj_test <- xml2::read_html('https://es.wikipedia.org/wiki/Anexo:Gabinete_de_Benito_Ju%C3%A1rez')
-
-
